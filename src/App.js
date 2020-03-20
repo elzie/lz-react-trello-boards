b590058f92aebaf8e6e20e59fab29148b13e9cfd
Djan K. Blom <djan@live.dk>
2020-03-20 20:08:22 +0100

    Modern React-app first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>I am getting awesome at React!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

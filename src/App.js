4c03e82df3f6d9500a6daa1a63c1702dc1959339
Djan K. Blom <djan@live.dk>
2020-03-20 23:02:24 +0100

    Basic setup + components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import Board from './components/Board';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Board />
         <img src={logo} className="App-logo" alt="logo" />
         <p>I am getting awesome at React!</p>
         <p>
---
+++ src/components/Board.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import List from './List';
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Board Component</p>
+
+                <List />
+
+            </div>
+        )
+    }
+}
+
+export default Board;

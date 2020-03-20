ed5ae5238fabf6b0f74f5b55c5a3e71b093ab0da
Djan K. Blom <djan@live.dk>
2020-03-20 23:25:05 +0100

    App component converted

--- src/App.js
+++ src/App.js
@@ -5,27 +5,17 @@ import './App.css';
 // Components
 import Board from './components/Board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Board />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>I am getting awesome at React!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    boardTitle: 'house ideas'
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.boardTitle}</p>
+      </div>
+    );
+  }
 }
 
 export default App;

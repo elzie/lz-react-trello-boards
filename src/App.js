b2ac8153f1a60af10740a80a8bd6bb3fcf985faf
Djan K. Blom <djan@live.dk>
2020-03-21 10:14:35 +0100

    Component lifecycle and looping

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './sampleData';
 
 // Components
 import Board from './components/Board';
 
 class App extends React.Component {
   state = {
-    boardTitle: 'house ideas'
+    boards: data.boards
   }
+  componentDidMount() {
+    this.setState({ boards: data.boards });
+
+  }
+
   render() {
     return (
       <div>
-        <p>{this.state.boardTitle}</p>
+        {
+          this.state.boards.map(board => (
+            <div key={board.id}>
+              <span>
+                {board.id}
+              </span>
+              <h3>{board.title}</h3>
+              <p>{board.background}</p>
+            </div>
+          ))
+        }
       </div>
     );
   }

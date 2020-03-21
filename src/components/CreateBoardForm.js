01154684d27f20937590e141ee5977c676ab9f7b
Djan K. Blom <djan@live.dk>
2020-03-21 15:34:16 +0100

    Controlled Components

---
+++ src/components/CreateBoardForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CreateBoardForm extends React.Component {
+    state = {
+        title: '',
+        background: '#80ccff'
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('form: ', this.state);
+        const board = {
+            title: this.state.title,
+            background: this.state.background,
+            createdAt: new Date(),
+            user: 'user'
+        }
+        if (board.title && board.background) {
+            this.props.createNewBoard(board);
+        }
+    }
+    render() {
+        return (
+            <div>
+                <p>Create Board Form</p>
+                <form
+                    className="create-board-wrapper"
+                    onSubmit={this.handleSubmit}
+                >
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Board name"
+                        onChange={(e) => this.setState({ title: e.target.value })}
+                    />
+                    <select name="background"
+                        onChange={(e) => this.setState({ background: e.target.value })}>
+                        <option value="#80ccff">Blue</option>
+                        <option value="#80ffaa">Green</option>
+                        <option value="#f94a1e">red</option>
+                        <option value="#ffb3ff">Pink</option>
+                        <option value="#bf00ff">Purple</option>
+                        <option value="#ffad33">Orange</option>
+                    </select>
+                    <button type="submit">Create new Board</button>
+                </form>
+            </div>
+        )
+    }
+}
+CreateBoardForm.propTypes = {
+    createNewBoard: PropTypes.func.isRequired
+}
+export default CreateBoardForm;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,31 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import BoardPreview from '../BoardPreview';
+import CreateBoardForm from '../CreateBoardForm';
 
 class Home extends React.Component {
-    newBoard = () => {
-        // Grab all data from our form - then call createNewBoard function
-        const board = {
-            title: 'Courses to Finish',
-            background: '#80ffaa',
-            createdAt: new Date()
-        }
-        this.props.createNewBoard(board);
-    }
+
     render() {
         return (
             <div>
                 <h1>Home Page</h1>
-                <button onClick={this.newBoard}>New Board</button>
-                {
-                    Object.keys(this.props.boards).map((key, index) => (
-                        // this.props.boards.map() gives "Cannot read property 'title' of undefined
-                        // which is because we are using .map(which is for Arrays) on an Object.
-                        // Object.keys() pulls out all of an objects property names and returns an array.
-                        <BoardPreview key={key} board={this.props.boards[key]} />
-                    ))
-                }
-                {console.log('home:', this.props.boards)}
+                <CreateBoardForm createNewBoard={this.props.createNewBoard} />
+                <div className="board-preview-wrapper">
+                    {
+                        Object.keys(this.props.boards).map((key, index) => (
+                            // this.props.boards.map() gives "Cannot read property 'title' of undefined
+                            // which is because we are using .map(which is for Arrays) on an Object.
+                            // Object.keys() pulls out all of an objects property names and returns an array.
+                            <BoardPreview key={key} board={this.props.boards[key]} />
+                        ))
+                    }
+                    {
+                        // console.log('home:', this.props.boards)
+                    }
+                </div>
             </div>
         )
     }

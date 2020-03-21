e4f40388378b39d08a8f5d2f9a8df0785d6c08d6
Djan K. Blom <djan@live.dk>
2020-03-21 15:57:16 +0100

    Create New Card form

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,27 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 class List extends React.Component {
+    state = {
+        currentCards: []
+    }
+
+    nameInput = React.createRef()
+
+    createNewCard = (e) => {
+        e.preventDefault();
+        const card = {
+            text: this.nameInput.current.value,
+            listId: 'abcd1234',
+            labels: [],
+            createdAt: new Date()
+        }
+        if (card.text) {
+            this.setState({ currentCards: [...this.state.currentCards, card] })
+        }
+        this.nameInput.current.value = '';
+        // console.log('new card added: ', card.text);
+    }
+
     render() {
         return (
             <div className="list">
@@ -15,6 +36,11 @@ class List extends React.Component {
                         key={key}
                         data={this.props.list.cards[key]} />
                 ))}
+                <form
+                    onSubmit={this.createNewCard}
+                    className="new-card-wrapper">
+                    <input type="text" name="name" placeholder="+ New card" ref={this.nameInput} />
+                </form>
             </div>
         )
     }
